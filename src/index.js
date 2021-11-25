5fa415d309cdad54e840ab8a63448b1bb6056d8e
virgiawankusuma <virgiawan150@gmail.com>
2021-11-25 14:15:46 +0700

    feat: installing font awesome and import to index.js

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import "regenerator-runtime";
+import "./style.css";
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+
+import '@fortawesome/fontawesome-free/js/all.js';
