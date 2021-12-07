23e5a2ab8e3a41e5899a48429c66e370d40f3576
virgiawankusuma <virgiawan150@gmail.com>
2021-12-08 02:13:49 +0700

    fix: change classify method

--- src/script/view/predict.js
+++ src/script/view/predict.js
@@ -1,16 +1,7 @@
 import * as tf from '@tensorflow/tfjs';
-// import * as mobilenet from '@tensorflow-models/mobilenet';
 
 let model;
 async function init() {
-    // const model = await tf.loadLayersModel('https://raw.githubusercontent.com/virgiawankusuma/API/master/model.json');
-    // const model = await tf.loadLayersModel('https://raw.githubusercontent.com/virgiawankusuma/API/master/New%20folder/model.json');
-    // const model = await tf.loadLayersModel('https://raw.githubusercontent.com/virgiawankusuma/API/master/modeljson/model.json');
-    // const model = await tf.loadLayersModel('https://raw.githubusercontent.com/virgiawankusuma/API/master/model/model.json');
-    // const model = await tf.loadLayersModel('https://raw.githubusercontent.com/virgiawankusuma/API/master/test/model.json');
-    // const model = await tf.loadLayersModel('https://raw.githubusercontent.com/virgiawankusuma/nlp-in-tfjs/master/model.json');
-    // console.log(model.summary());
-
     console.log("model loading..");
     model = undefined;
     model = await tf.loadLayersModel('https://raw.githubusercontent.com/virgiawankusuma/API/master/model.json');
@@ -32,15 +23,11 @@ const IMAGENET_CLASSES = {
     11: 'White Glass',
 }
 
-async function classify(imgClassified) {
-
-    console.log("model loading..");
-
-    console.log(model);
-    let tensor = preprocessImage(imgClassified, 'mobilenet');
-
-    let predictions = await model.predict(tensor).data();
-    let results = Array.from(predictions)
+async function classify(image) {
+    // action for the submit button
+    let tensorImg = tf.browser.fromPixels(image).resizeNearestNeighbor([224, 224]).toFloat().expandDims();
+    let prediction = await model.predict(tensorImg).data();
+    let results = Array.from(prediction)
         .map(function (p, i) {
             return {
                 probability: p,
@@ -50,29 +37,7 @@ async function classify(imgClassified) {
             return b.probability - a.probability;
         }).slice(0, 5);
 
-    // console.log(results[0].className);
-    // console.log(results[1].className);
-    // console.log(results[2].className);
-    // console.log(results[3].className);
-    // console.log(results[4].className);
     console.log(JSON.stringify(results));
-}
-
-function preprocessImage(imgClassified, modelName) {
-    let tensor = tf.browser.fromPixels(imgClassified)
-        .resizeNearestNeighbor([224, 224])
-        .toFloat();
-
-    if (modelName === undefined) {
-        return tensor.expandDims();
-    } else if (modelName === "mobilenet") {
-        let offset = tf.scalar(127.5);
-        return tensor.sub(offset)
-            .div(offset)
-            .expandDims();
-    } else {
-        alert("Nama model tidak diketahui..")
-    }
 }
 
 export { init, classify };
