c31cd7068be96fa5b60bca87413fadab4a09878f
tnnz20 <kaitora200@gmail.com>
2021-12-09 22:04:43 +0800

    fix : clean code

--- src/script/view/predict.js
+++ src/script/view/predict.js
@@ -2,44 +2,52 @@ import * as tf from '@tensorflow/tfjs';
 
 let model;
 async function init() {
-    console.log("model loading..");
-    model = undefined;
-    model = await tf.loadLayersModel('https://raw.githubusercontent.com/virgiawankusuma/API/master/model.json');
-    console.log("model loaded..");
+	console.log('model loading..');
+	model = undefined;
+	model = await tf.loadLayersModel(
+		'https://raw.githubusercontent.com/virgiawankusuma/API/master/model.json'
+	);
+	console.log('model loaded..');
 }
 
 const IMAGENET_CLASSES = {
-    0: 'Battery',
-    1: 'Biological',
-    2: 'Brown Glass',
-    3: 'Cardboard',
-    4: 'Clothes',
-    5: 'Green Glass',
-    6: 'Metal',
-    7: 'Paper',
-    8: 'Plastic',
-    9: 'Shoes',
-    10: 'Trash',
-    11: 'White Glass',
-}
+	0: 'Battery',
+	1: 'Biological',
+	2: 'Brown Glass',
+	3: 'Cardboard',
+	4: 'Clothes',
+	5: 'Green Glass',
+	6: 'Metal',
+	7: 'Paper',
+	8: 'Plastic',
+	9: 'Shoes',
+	10: 'Trash',
+	11: 'White Glass',
+};
 
 async function classify(image) {
-    // action for the submit button
-    let tensorImg = tf.browser.fromPixels(image).resizeNearestNeighbor([224, 224]).toFloat().expandDims();
-    let prediction = await model.predict(tensorImg).data();
-    let results = Array.from(prediction)
-        .map(function (p, i) {
-            return {
-                probability: p,
-                className: IMAGENET_CLASSES[i]
-            };
-        }).sort(function (a, b) {
-            return b.probability - a.probability;
-        }).slice(0, 5);
+	// action for the submit button
+	let tensorImg = tf.browser
+		.fromPixels(image)
+		.resizeNearestNeighbor([224, 224])
+		.toFloat()
+		.expandDims();
+	let prediction = await model.predict(tensorImg).data();
+	let results = Array.from(prediction)
+		.map(function (p, i) {
+			return {
+				probability: p,
+				className: IMAGENET_CLASSES[i],
+			};
+		})
+		.sort(function (a, b) {
+			return b.probability - a.probability;
+		})
+		.slice(0, 5);
 
-    console.log(JSON.stringify(results));
-    const jensam = document.getElementById('jensam');
-    jensam.innerText = `[${results[0].className}]`;
+	console.log(JSON.stringify(results));
+	const jensam = document.getElementById('jensam');
+	jensam.innerText = `[${results[0].className}]`;
 }
 
 export { init, classify };
