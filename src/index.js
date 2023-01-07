d6004794b14a30661d52aca0ad51bacba07084e2
virgiawankusuma <virgiawan150@gmail.com>
2023-01-08 02:08:33 +0700

    feat: init project with react

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
-import "regenerator-runtime";
-import "./style.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.min.js';
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
-import '@fortawesome/fontawesome-free/js/all.js';
-
-import main from './script/view/main.js';
-
-document.addEventListener("DOMContentLoaded", main);
-
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
