70ec757cebd4e2fefa9bc60fd6b6a16628d2e18c
virgiawankusuma <virgiawan150@gmail.com>
2023-01-15 22:59:02 +0700

    feat: Making Router Schema

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import { 
+  Routes, 
+  Route 
+} from 'react-router-dom';
+
+import Home from './pages/Home';
+import Acknowledgment from './pages/Acknowledgment';
+import Classify from './pages/Classify';
+
 function App() {
   return (
-    <>
-    </>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/acknowledgment" element={<Acknowledgment />} />
+      <Route path="/klasifikasikan" element={<Classify />} />
+    </Routes>
   );
 }
 
