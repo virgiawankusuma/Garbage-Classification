2314599d155dcba5bd4dceb0a304fa8de170ebe9
virgiawankusuma <virgiawan150@gmail.com>
2023-01-16 23:57:05 +0700

    fix: Adjust responsivity

--- src/components/aboutSection/About.js
+++ src/components/aboutSection/About.js
@@ -5,15 +5,15 @@ export default function About() {
     <section id="about" className="pt-36">
     <div className="container">
       <div className="flex flex-wrap justify-center">
-        <h2 className="text-2xl text-dark font-bold text-center w-full mb-10 md:text-3xl lg:text-5xl">
+        <h2 className="text-2xl text-dark font-bold text-center w-full mb-6 md:text-3xl lg:text-5xl">
           {about.title}
         </h2>
         <img
           src={about.image}
           alt="about illustration"
-          className="w-full lg:w-3/4"
+          className="w-full lg:w-[75%]"
         />
-        <p className="w-full text-dark font-medium text-center mb-10 lg:leading-relaxed lg:w-3/4 lg:text-xl">
+        <p className="w-full text-dark font-medium text-center mb-10 lg:leading-relaxed lg:w-2/3 lg:text-lg">
           {about.desc}
         </p>
       </div>
