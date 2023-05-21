8d8002f5a1b331af0de30debc2482d8cd7897d76
Virgiawan Teguh Kusuma <virgiawan150@gmail.com>
2023-05-21 23:07:34 +0700

    Update webpack.common.js

--- webpack.common.js
+++ webpack.common.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
     entry: "./src/index.js",
     output: {
-        path: path.resolve(__dirname, "dist"),
+        path: path.resolve(__dirname, "build"),
         filename: "bundle.js"
     },
     module: {
